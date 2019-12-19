31e5d1fc5d291b863edf4ed43972842dcb1957de
Craig Swanson <cswanson02@me.com>
2019-12-19 00:10:01 -0600

    Created skeleton of model representations file.

---
+++ KidsFly/KidsFly/Models/ModelRepresentations.swift
@@ -0,0 +1,21 @@
+//
+//  ModelRepresentations.swift
+//  KidsFly
+//
+//  Created by Craig Swanson on 12/19/19.
+//  Copyright © 2019 Craig Swanson. All rights reserved.
+//
+
+import Foundation
+
+struct TravelerRepresentation: Equatable, Codable {
+    
+}
+
+struct KFConnectionRepresentation: Equatable, Codable {
+    
+}
+
+struct TripsRepresentation: Equatable, Codable {
+    
+}
